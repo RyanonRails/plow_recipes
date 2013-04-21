162a1b0610e65af41f0d97d6510d08da748844d1
Ryan Jones <ryan@system88.com>
2013-04-20 22:03:10 -0600

    Update postgres.sh
    continue adding repo (skip over warning)

--- postgres.sh
+++ postgres.sh
@@ -7,7 +7,7 @@
 if dpkg -s "postgresql-9.2"; then
   echo 'postgresql-9.2 already installed'
 else
-  add-apt-repository ppa:pitti/postgresql
+  add-apt-repository ppa:pitti/postgresql -y
   apt-get update
   apt-get install -y postgresql-9.2 libpq-dev
 fi
